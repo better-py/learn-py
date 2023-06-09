40d4b7c9b4c6d4e4f53c3ed964e64c42e9e17927
Henry.Huang <hhglory@outlook.com>
2023-06-10 02:00:09 +0800

    update

--- packages/py-try/try-nicegui/src/try_nicegui/run03_exe.py
+++ packages/py-try/try-nicegui/src/try_nicegui/run03_exe.py
@@ -1,15 +1,14 @@
 import sys
 from nicegui import ui
-
+from nicegui import app, ui
 ui.label('Hello from PyInstaller')
 
 # sys.stdout = open('logs.log', 'w')
+app.native.window_args['resizable'] = False
+app.native.start_args['debug'] = False
 
 
 ui.run(
     native=True,
     window_size=(500, 400),
-    fullscreen=False,
-    reload=False,
-    show=False
 )
