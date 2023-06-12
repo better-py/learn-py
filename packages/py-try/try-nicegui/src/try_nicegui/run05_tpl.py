522bf0dfdb777ea2ac5320c8c999ee145adc5b9f
Henry.Huang <hhglory@outlook.com>
2023-06-12 09:36:00 +0800

    update

--- packages/py-try/try-nicegui/src/try_nicegui/run05_tpl.py
+++ packages/py-try/try-nicegui/src/try_nicegui/run05_tpl.py
@@ -6,6 +6,10 @@ def new_tab_view():
     div.classes('row w-full h-full')
     div.tailwind().columns("1").width("screen").height("screen")
 
+    # 底部浮动按钮:
+    # with ui.page_sticky(position='bottom-right', x_offset=20, y_offset=20):
+    #     ui.button(on_click=None).props('fab icon=contact_support')
+
     with div:
         sp = ui.splitter(value=20)  # 切分分栏比例
 
@@ -15,7 +19,7 @@ def new_tab_view():
                 d.tailwind().columns("1").width("screen").height("screen")
 
                 with d:
-                    ui.menu_item('Menu item 1', lambda: ui.notify("hi!", color="orange", position="top"))
+                    ui.menu_item('Menu item 1', lambda: ui.open('/page1'))
                     ui.menu_item('Menu item 1', lambda: ui.notify("hi!", color="green", position="top"))
 
             with sp.after:
@@ -32,8 +36,21 @@ def home_page(client: Client):
     div.tailwind().padding("0")
 
     with div:
-        with ui.header(value=True):
-            ui.label('Header')
+        h = ui.header(value=True)
+        h.tailwind.align_items("center")  # 元素居中
+        drawer = ui.left_drawer().classes("bg-gray-900 text-white")  # 提前定义
+
+        with h:
+            ui.label('Header').classes("center")
+            ui.button(on_click=lambda: drawer.toggle()).props(
+                "flat color=white icon=menu round size=sm"
+            )
+
+            ui.link('Home', '/').classes("text-red-500 text-solid")
+
+        with drawer:  # top_corner=True, bottom_corner=True
+            ui.menu_item('Menu item 1', lambda: ui.open('/page1'))
+            ui.menu_item('Menu item 1', lambda: ui.notify("hi!", color="green", position="top"))
 
         #
         # main content:
@@ -44,13 +61,47 @@ def home_page(client: Client):
             ui.label('Footer')
 
 
-@ui.page('/page1')
-def new_page1():
+def new_page1_view():
     with ui.card():
-        ui.label('page1')
+        ui.label("this is page 1 view")
+
+
+@ui.page('/page1')
+def new_page1(client: Client):
+    client.content.classes(remove='nicegui-content')  # 去除干扰样式！
+
+    div = ui.element('div')
+    div.classes("column p-0 m-0 gap-0")
+    div.tailwind().padding("0")
+
+    with div:
+        drawer = ui.left_drawer().classes("bg-pink-300")  # 提前定义
+
+        with ui.header(value=True):
+            ui.button(on_click=lambda: drawer.toggle()).props(
+                "flat color=white icon=menu round size=sm"
+            ).classes()
+            ui.label('Header')
+            ui.link('Home', '/')
+            ui.button(on_click=ui.left_drawer.hide).props(
+                "flat color=white icon=menu round"
+            ).classes("lg:hidden")
+
+        with drawer:  # top_corner=True, bottom_corner=True
+            ui.menu_item('Menu item 1', lambda: ui.open('/page1'))
+            ui.menu_item('Menu item 1', lambda: ui.notify("hi!", color="green", position="top"))
+
+        #
+        # main content:
+        #
+        new_page1_view()
+
+        with ui.footer(value=True):
+            ui.label('Footer')
 
 
 ui.run(
+    native=True,
     window_size=(1100, 700),
     fullscreen=False,
     reload=True,
