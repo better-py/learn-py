1b6ed1d67809e89463537d8166b1df394e53b418
Henry.Huang <hhglory@outlook.com>
2023-08-01 10:08:19 +0800

    update

--- packages/py-try/try-spacy/src/try_spacy/run01.py
+++ packages/py-try/try-spacy/src/try_spacy/run01.py
@@ -1,4 +1,5 @@
 import spacy
+from spacy import displacy
 
 
 def nlp_print():
@@ -21,6 +22,34 @@ def nlp_print2():
     print(token.morph.get("PronType"))  # ['Prs']
 
 
+def nlp_print3():
+    text = """When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the
+    company took him seriously. “I can tell you very senior CEOs of major American car companies would shake my hand
+    and turn away because I wasn’t worth talking to,” said Thrun, now the co-founder and CEO of online higher
+    education startup Udacity, in an interview with Recode earlier this week.
+
+    A little less than a decade later, dozens of self-driving startups have cropped up while automakers around the
+    world clamor, wallet in hand, to secure their place in the fast-moving world of fully automated transportation.
+
+    """
+
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+
+    # 彩色显示句子成分
+    displacy.serve(doc, style="ent", host="127.0.0.1", auto_select_port=True)
+
+
+def nlp_print4():
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp("This is a sentence.")
+
+    # 依赖关系图
+    displacy.serve(doc, style="dep", host="127.0.0.1", auto_select_port=True)
+
+
 if __name__ == "__main__":
     nlp_print()
     nlp_print2()
+    nlp_print3()
+    # nlp_print4()
