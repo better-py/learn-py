15c1cbf1f51ce2fccdf90202b76cc13bb22f6e02
Henry.Huang <hhglory@outlook.com>
2023-12-04 23:54:56 +0800

    update

---
+++ packages/py-try/try-ccxt/src/try_ccxt/tr_binance.py
@@ -0,0 +1,28 @@
+import ccxt
+from loguru import logger
+
+
+def try_ccxt():
+    logger.info(f'exchanges: {ccxt.exchanges}')
+
+    exchange = ccxt.binance(config={
+        'timeout': 200000,
+
+        # use local http proxy
+        'proxies': {
+            'http': 'http://127.0.0.1:7890',
+            'https': 'http://127.0.0.1:7890',
+        }
+    })
+
+    # logger.info(f"exchange: {exchange.describe()}")
+
+    # order books
+    for symbol in ['BTC/USDT', 'ETH/BTC', 'ETH/USDT']:
+        ob = exchange.fetch_order_book(symbol, limit=10)
+        logger.info(f"order book: {ob}")
+
+
+if __name__ == '__main__':
+    try_ccxt()
+    pass
