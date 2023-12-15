00d61c3f64eebce6b2852f60e3dad145b123bdb2
Henry.Huang <hhglory@outlook.com>
2023-12-16 01:37:18 +0800

    update

---
+++ packages/py-ai/try-gemini/README.md
@@ -0,0 +1,11 @@
+# Google AI LLM:
+
+- Google AI 大模型: Gemini
+
+## 参考示例代码:
+
+- https://github.com/GoogleCloudPlatform/generative-ai/blob/main/gemini/getting-started/intro_gemini_python.ipynb
+
+## Reference:
+
+- https://github.com/google/generative-ai-python
--- packages/py-ai/try-gemini/src/try_gemini/run.py
+++ packages/py-ai/try-gemini/src/try_gemini/run.py
@@ -14,8 +14,24 @@ def run(api_key: str):
     genai.configure(api_key=api_key)
     model = genai.GenerativeModel("gemini-pro")
 
+    # usage 1:
     response = model.generate_content("The opposite of hot is")
     logger.info(f"response: {response.text}")  # cold.
+
+    # usage 2:
+    responses = model.generate_content("Why is the sky blue?", stream=True)
+    for response in responses:
+        logger.info(f"response: {response.text}")
+
+    # usage 3:
+    prompt = """Create a numbered list of 10 items. Each item in the list should be a trend in the tech industry.
+
+    Each trend should be less than 5 words."""  # try your own prompt
+
+    responses = model.generate_content(prompt, stream=True)
+
+    for response in responses:
+        print(response.text, end="")
 
 
 if __name__ == '__main__':
