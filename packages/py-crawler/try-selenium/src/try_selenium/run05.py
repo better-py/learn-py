442d4f3e5a7fdfeed4cd12851269826e9d758ff7
Henry.Huang <hhglory@outlook.com>
2024-02-25 20:22:42 +0800

    update

---
+++ packages/py-crawler/try-selenium/src/try_selenium/run05.py
@@ -0,0 +1,284 @@
+import json
+import os
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+# from playsound import playsound
+
+# 本程序为美签slot查询(英国：伦敦，贝尔法斯特)，可以根据期望的日期和刷新频率来刷新美签slot(英国：伦敦，贝尔法斯特)。
+# 本程序仅限英国美签slot查询，大家也可以照猫画虎修改成自己所在的国家美签申请中心。
+# 祝大家能够抢到自己心仪的的slot！
+
+
+HOST_URL = ""
+
+#
+LOGIN_GB_URL = "https://ais.usvisa-info.com/en-gb/niv/users/sign_in"
+LOGIN_CA_URL = "https://ais.usvisa-info.com/en-ca/niv/users/sign_in"
+
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+
+
+#
+#
+#
+
+def get_schedule_api(country_id: str, schedule_id: str, location_id: str):
+    # ca = canada, gb = england
+    # 95 = vancouver
+    return f"https://ais.usvisa-info.com/en-{country_id or 'ca'}/niv/schedule/{schedule_id}/appointment/days/{location_id or 95}.json?appointments[expedite]=false"
+
+
+# authToken non-login session are needed for further login processes.(authToken和未登录时的session获取，为登录作准备)
+def getInfo():
+    headers = {
+        'User-Agent': USER_AGENT
+    }
+
+    session = requests.Session()
+    response = session.get(LOGIN_CA_URL, headers=headers).text
+
+    preSession = session.cookies
+
+    soup = BeautifulSoup(response, 'lxml')
+
+    auth_token = soup.find("meta", {"name": "csrf-token"})['content']
+    print(f"http header csrf-token: {auth_token}")
+
+    return auth_token, preSession, USER_AGENT
+
+
+# send login request, and gain cookie after login. (发送登录请求，获取登录后的cookie)
+def login(email, password):
+    response = getInfo()
+    authToken = response[0]
+    preSession = response[1]
+    userAgent = response[2]
+
+    name = ''
+    value = ''
+    for c in preSession:
+        name = c.name
+        value = c.value
+    cookie = name + "=" + value
+
+    payload = {
+        'utf8': '✓',
+        'user[email]': email,
+        'user[password]': password,
+        'policy_confirmed': '1',
+        'commit': 'Sign In',
+        'authenticity_token': authToken
+    }
+
+    # 请请求头尽可能模拟浏览器
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Host': 'ais.usvisa-info.com',
+        'Origin': 'https://ais.usvisa-info.com',
+        'Referer': 'https://ais.usvisa-info.com/en-gb/niv/users/sign_in',
+        'Update-Insecure-Requests': '1',
+        'User-Agent': userAgent,
+        'Cookie': cookie
+    }
+
+    loginSession = requests.Session()
+
+    loginResponse = loginSession.post(LOGIN_CA_URL, headers=headers, data=payload)
+
+    print(f"login response: {loginResponse.text}")
+
+    loginSessionName = ''
+    loginSessionValue = ''
+
+    print(f"cookie: {loginSession.cookies}")
+
+    for c in loginResponse.cookies:
+        loginSessionName = c.name
+        loginSessionValue = c.value
+        break
+
+    return loginSessionName + "=" + loginSessionValue
+
+
+# 发生错误并重试
+def retry():
+    print('发生了一些错误或者会话过期，5秒后重试。。。')
+    time.sleep(5)
+
+    execute()
+
+
+# 邮件推送
+
+
+def execute():
+    counter = 0
+
+    data = {}
+
+    # 读取 JSON文件
+    # with open('data.json', 'r') as dataFile:
+    #     data = json.load(dataFile)
+
+    #
+    # read env here!
+    #
+
+    email = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+    scheduleID = os.getenv("SCHEDULEID")
+
+    ######################################################
+
+    email = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+    scheduleID = os.getenv("SCHEDULE_ID")
+    expectedDate = os.getenv("EXPECTEDDATE") or "2024-07-10"
+
+    if not email or not password or not scheduleID:
+        print("Please set EMAIL, PASSWORD, and SCHEDULEID environment variables")
+        return
+
+    print(f"email: {email}, password: {password}, scheduleID: {scheduleID}, expectedDate: {expectedDate}")
+
+    sleepTime = 5
+    userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+
+    ######################################################
+
+    #
+    #
+    # 登录
+    cookie = login(email, password)
+
+    while True:
+
+        try:
+            # 请请求头尽可能模拟浏览器
+            # construct headers (DO NOT MODIFY THESE REQUEST HEADERS IF YOU DON'T FAMILIAR WITH HTTP REQUEST 请勿随意修改请求头)
+            headers = {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,'
+                          '*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Cache-Control': 'max-age=0',
+                'Cookie': cookie,
+                'Connection': 'keep-alive',
+                'Host': 'ais.usvisa-info.com',
+                'Upgrade-Insecure-Requests': '1',
+
+                # 如果输出错误/HTML代码，可能是User-Agent的问题。
+                # User-Agent 根据不同的系统而不同，我用的是MacOS的User-Agent。
+                # 如果在Windows运行，通常用：
+                # Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36
+                # Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36
+                # Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36
+                # 这三个都可以。
+                # 如果是MacOS，以下有几个通常用的例子：
+                # Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36
+                # Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36
+                # Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36
+                #
+                # 如果以上的例子乱码还会出现，那么就可以按照以下方法做：
+                # 在浏览器打开F12切换到network栏，在签证官网刷新页面随便点一个request，下拉看到request
+                # header里面的user - agent右键复制。然后在main.py里面把user - agent的值删除然后粘贴即可。
+
+                'User-Agent': userAgent
+            }
+
+            londonSession = requests.Session()
+
+            # send requests and get london and belfast schedule responses
+            spot1_resp = londonSession.get(get_schedule_api("ca", scheduleID, "95"), headers=headers)
+            spot2_resp = requests.get(get_schedule_api("ca", scheduleID, "94"), headers=headers)
+
+            #
+            #
+            #
+
+            print(f"spot 1 response: {spot1_resp.text}")
+            print(f"spot 2 response: {spot2_resp.text}")
+
+            print("时间: " + str(time.asctime()))
+
+            # london slot check
+            earliestLondon = spot1_resp.text[10:20]
+            print('London:')
+            if len(spot1_resp.text) == 2:
+                print("呜呜呜伦敦slot都没有啦")
+            elif earliestLondon == "Your sessi" or earliestLondon == "You need t":
+                retry()
+            elif earliestLondon > expectedDate:
+                print('可预定但超过期望时间：' + earliestLondon)
+                print('链接: ' + 'https://ais.usvisa-info.com/en-egb/niv/schedule/' + scheduleID + '/appointment')
+            elif earliestLondon <= expectedDate:
+                print('！！可预定！！London 最早可预定时间: ' + earliestLondon)
+                print('链接: ' + 'https://ais.usvisa-info.com/en-gb/niv/schedule/' + scheduleID + '/appointment')
+                mailContent = '！！可预定！！London 最早可预定时间: ' + earliestLondon + '\n' + '链接: ' + 'https://ais.usvisa-info.com/en-gb/niv/schedule/' + scheduleID + '/appointment'
+
+                #
+                #
+                #
+                # todo x: send notification here!
+                print(f"Sending notification: {mailContent}")
+
+            else:
+                retry()
+
+            # belfast slot check
+            earliestBelfast = spot2_resp.text[10:20]
+            print('Belfast:')
+            if len(spot2_resp.text) == 2:
+                print("呜呜呜贝法slot都没有啦\n")
+            elif earliestBelfast == "Your sessi" or earliestBelfast == "You need t":
+                retry()
+            elif earliestBelfast > expectedDate:
+                print('可预定但超过期望时间：' + earliestBelfast)
+                print(
+                    '链接: ' + 'https://ais.usvisa-info.com/en-gb/niv/schedule/' + scheduleID + '/appointment' + '\n')
+            elif earliestBelfast <= expectedDate:
+                print('！！可预定！！Belfast 最早可预定时间: ' + earliestBelfast)
+                print(
+                    '链接: ' + 'https://ais.usvisa-info.com/en-gb/niv/schedule/' + scheduleID + '/appointment' + '\n')
+                mailContent = '！！可预定！！Belfast 最早可预定时间: ' + earliestBelfast + '\n' + '链接: ' + 'https://ais.usvisa-info.com/en-gb/niv/schedule/' + scheduleID + '/appointment'
+
+                #
+                #
+                #
+                # todo x: send notification here!
+                print(f"Sending notification: {mailContent}")
+            else:
+                retry()
+
+            # refresh cookie and store to JSON file
+            # 循环counter次后更新session以保持登录状态，这里可以改刷新频率。但是请注意经过测试session过期时间大概为30分钟，刷新频率最好不要大于30min/次
+            counter = counter + 1
+            if counter == 10:
+                londonCookie = spot1_resp.cookies
+                newLondonCookiePart = json.dumps(requests.utils.dict_from_cookiejar(londonCookie)).split("\"")[3]
+                newLondonCookie = "_yatri_session=" + newLondonCookiePart
+                cookie = newLondonCookie
+
+                counter = 0
+
+            # sleep time (少于5秒可能会造成TooManyRequests Error,建议sleep时间>=5秒)
+            time.sleep(sleepTime)
+        # catch errors and retry
+        # 如果一些不可预测的bug发生,隔5秒重试。
+        except Exception as e:
+            print('发生错误：')
+            print(e.__class__)
+            print(e)
+            retry()
+
+
+# 主函数
+if __name__ == '__main__':
+    execute()
---
+++ packages/py-crawler/try-selenium/src/try_selenium/run06.py
@@ -0,0 +1,134 @@
+import os
+
+import requests
+from bs4 import BeautifulSoup
+from loguru import logger
+from requests.utils import dict_from_cookiejar
+
+LOGIN_URL = ""
+LOGIN_CA_URL = "https://ais.usvisa-info.com/en-ca/niv/users/sign_in"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+
+
+def get_schedule_api(country_id: str, schedule_id: str, location_id: str):
+    # ca = canada, gb = england
+    # 95 = vancouver
+
+    json_api = "https://ais.usvisa-info.com/en-ca/niv/schedule/53081715/appointment/days/94.json?appointments[expedite]=false"
+
+    return f"https://ais.usvisa-info.com/en-{country_id or 'ca'}/niv/schedule/{schedule_id}/appointment/days/{location_id or 95}.json?appointments[expedite]=false"
+
+
+def pre_login(lang_code, country_code):
+    host = f"https://ais.usvisa-info.com/{lang_code or 'en'}-{country_code or 'ca'}/niv/users/sign_in"
+    headers = {
+        'User-Agent': USER_AGENT,
+    }
+
+    logger.info(f"login url: {host}")
+
+    session = requests.Session()
+    resp = session.get(host, headers=headers).text
+
+    pre_session = session.cookies
+
+    for cookie in session.cookies:
+        logger.info(f"http cookie: {cookie.name}={cookie.value}")
+
+    soup = BeautifulSoup(resp, 'lxml')
+
+    csrf_token = soup.find("meta", {"name": "csrf-token"})['content']
+
+    logger.info(f"http header csrf-token: {csrf_token}")
+
+    # do login
+
+    return host, pre_session, csrf_token
+
+
+def login(email, password, schedule_id, location_id):
+    lang_code = 'en'
+    country_code = 'ca'
+
+    # prepare for login
+    host, cookies, csrf_token = pre_login(lang_code, country_code)
+
+    # 请请求头尽可能模拟浏览器
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Host': 'ais.usvisa-info.com',
+        'Origin': 'https://ais.usvisa-info.com',
+        'Referer': host,
+        'Update-Insecure-Requests': '1',
+        'User-Agent': USER_AGENT,
+        # 'Cookie': cookie,
+
+        #
+        #
+        #
+        'Dnt': '1',
+        'Sec-Ch-Ua-Platform': '"macOS"',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'X-Csrf-Token': csrf_token,
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+
+    payload = {
+        'user[email]': email,
+        'user[password]': password,
+        'policy_confirmed': '1',
+        'commit': 'Sign In',
+    }
+
+    session = requests.Session()
+
+    resp = session.request(
+        'POST',
+        host,
+        headers=headers,
+        cookies=cookies,
+        data=payload
+    )
+
+    for cookie in session.cookies:
+        logger.info(f"http login cookie: {cookie.name}={cookie.value}")
+
+    auth_cookies = dict_from_cookiejar(session.cookies)
+    logger.info(f"login resp cookies: {auth_cookies}")
+
+    logger.info(f"login resp: {resp.status_code}, ")
+
+    #
+    # check schedule
+    #
+    check_url = get_schedule_api(country_code, 'SCHEDULE_ID', 'LOCATION_ID')
+
+    logger.info(f"check url: {check_url}")
+
+    r = session.get(check_url, headers=headers)
+
+    logger.info(f"check resp: {r.status_code}, {r.text}")
+
+
+def main():
+    email = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+    schedule_id = os.getenv("SCHEDULE_ID")
+    location_id = os.getenv("LOCATION_ID") or "95"  # vancouver
+
+    if not email or not password:
+        print("Please set EMAIL and PASSWORD environment variables")
+        return
+
+    logger.info(f"email: {email}, password: {password}, schedule_id: {schedule_id}")
+
+    login(email, password, schedule_id, location_id)
+
+
+if __name__ == '__main__':
+    main()
