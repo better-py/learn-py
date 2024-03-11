6842e4976ee3e3a5d1967f94ac881cb5cc9b64d3
Henry.Huang <hhglory@outlook.com>
2024-03-11 20:55:31 +0800

    update

--- packages/py-quant/try-ccxt/README.md
+++ packages/py-quant/try-ccxt/README.md
@@ -8,3 +8,8 @@
 
 - ask: 本地测试,频繁出现请求超时.
 - ans: 需要设置 HTTP 代理.
+
+### proxy + 403 Forbidden:
+
+- ask: 本地测试, 403 Forbidden
+- ans: VPN 不要使用 HK 节点, 改为 JP, TW 节点.
--- packages/py-quant/try-ccxt/src/try_ccxt/tr_binance.py
+++ packages/py-quant/try-ccxt/src/try_ccxt/tr_binance.py
@@ -1,26 +1,58 @@
+import datetime
+import os
+
 import ccxt
 from loguru import logger
 
 
-def try_ccxt():
-    logger.info(f'exchanges: {ccxt.exchanges}')
+def new_bn_client(api_key, api_secret, use_proxy=True):
+    logger.info(f"binance api key {api_key}, secret {api_secret}")
 
-    exchange = ccxt.binance(config={
-        'timeout': 200000,
+    return ccxt.binance({
+        'apiKey': api_key,
+        'secret': api_secret,
+        'enableRateLimit': True,
+        # 'verbose': True,
+        'timeout': 4000,  # 4s
 
-        # use local http proxy
+        # local proxy:
+        #   TODO X: 本地不要使用 HK Proxy, 用 JP/TW Proxy! 否则报 403 Forbidden
         'proxies': {
             'http': 'http://127.0.0.1:7890',
             'https': 'http://127.0.0.1:7890',
-        }
+        } if use_proxy else None
+
     })
 
+
+def try_ccxt():
+    ts = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d_%H%M%S')
+    logger.add(f"tmp/try_ccxt_{ts}.log")  # todo x: log file
+
+    api_key = os.getenv("BINANCE_API_KEY")
+    api_secret = os.getenv("BINANCE_SECRET_KEY")
+
+    logger.info(f"binance api_key: {api_key}, api_secret: {api_secret}")
+
+    if not api_key or not api_secret:
+        logger.error("please set BINANCE_API_KEY and BINANCE_SECRET_KEY")
+        return
+
+    ex = new_bn_client(api_key, api_secret)
+
+    # get balance:
     # logger.info(f"exchange: {exchange.describe()}")
+    res = ex.fetch_balance()
+
+    # logger.info(f"balance: {res}")
+
+    resp = ex.fetch_open_orders(symbol="DOT/FDUSD")
+    logger.info(f"open orders: {resp}")
 
     # order books
-    for symbol in ['BTC/USDT', 'ETH/BTC', 'ETH/USDT']:
-        ob = exchange.fetch_order_book(symbol, limit=10)
-        logger.info(f"order book: {ob}")
+    # for symbol in ['BTC/USDT', 'ETH/BTC', 'ETH/USDT']:
+    #     ob = exchange.fetch_order_book(symbol, limit=10)
+    #     logger.info(f"order book: {ob}")
 
 
 if __name__ == '__main__':
