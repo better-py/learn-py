1f7e31ca745bfd54813f3421a92b9327d014d138
Henry.Huang <hhglory@outlook.com>
2024-03-15 21:38:30 +0800

    update

--- packages/py-data/try-pygwalker/src/try_pygwalker/run01.py
+++ packages/py-data/try-pygwalker/src/try_pygwalker/run01.py
@@ -1,11 +1,22 @@
+import os
+
 import gradio as gr
 import pandas as pd
-
+from loguru import logger
 from pygwalker.api.gradio import PYGWALKER_ROUTE, get_html_on_gradio
 
 with gr.Blocks() as demo:
+    logger.debug(f"{os.getcwd()}")
+
     df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
-    pyg_html = get_html_on_gradio(df, spec="/Users/douding/Desktop/github/pygwalker-in-streamlit/gw_config.json",
+
+    filename = f"{os.getcwd()}/tmp/gw_config.json"
+    if not os.path.exists(filename):
+        # touch file
+        # open(filename, 'a').close()
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+    pyg_html = get_html_on_gradio(df, spec=filename,
                                   spec_io_mode="rw")
     gr.HTML(pyg_html)
 
