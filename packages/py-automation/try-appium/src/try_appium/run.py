9f6a5fde0b37f4e55322b257d471b8d3dc74cfb8
Henry.Huang <hhglory@outlook.com>
2024-06-17 00:37:45 +0800

    update

---
+++ packages/py-automation/try-appium/src/try_appium/run.py
@@ -0,0 +1,41 @@
+from appium import webdriver
+
+
+def main():
+    appium_server_url = 'http://localhost:4723'
+
+    #
+    # 大麦 app
+    #
+    appID = "cn.damai.iphone"
+
+    # 配置 Appium 服务器和 iOS 设备的能力
+    desired_caps = {
+        'platformName': 'iOS',
+        'platformVersion': '15.8.2',  # 你的设备的 iOS 版本
+        'deviceName': 'iPhone 7',  # 你的设备名称
+        'udid': 'f9d3ab4de8ffbc4b188286a9623df43726d5495c',  # 你的设备 UDID
+        'automationName': 'XCUITest',
+        'bundleId': appID,  # 大麦 app 的 bundleId
+        'noReset': True,
+    }
+
+    # 创建 WebDriver 实例
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+    # 等待应用启动
+    time.sleep(5)
+
+    # 示例操作：打印当前应用的页面源代码
+    page_source = driver.page_source
+    print(page_source)
+
+    # 等待 2 秒
+    time.sleep(2)
+
+    # 退出测试并关闭应用
+    driver.quit()
+
+
+if __name__ == '__main__':
+    main()
