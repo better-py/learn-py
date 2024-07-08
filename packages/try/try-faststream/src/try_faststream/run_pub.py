f86958654e63c07c1a7b7096ffd0edb998a2bf69
Henry.Huang <hhglory@outlook.com>
2024-07-08 15:41:11 +0800

    update

--- packages/try/try-faststream/src/try_faststream/run_pub.py
+++ packages/try/try-faststream/src/try_faststream/run_pub.py
@@ -1,16 +1,30 @@
 import asyncio
 
 from faststream.nats import NatsBroker
+from loguru import logger
 
 
 async def pub():
     host = "nats://localhost:4222"
 
     async with NatsBroker(host) as broker:
+        msg = "Hi!"
+        logger.debug(f"publish message: {msg}")
         await broker.publish(
-            "Hi!",
+            msg,
             subject="test",
         )
+
+        #
+        # to one worker:
+        #
+        for i in range(10):
+            msg = f"msg {i}"
+            logger.debug(f"publish message: {msg}")
+            await broker.publish(
+                msg,
+                subject="test-workers",
+            )
 
 
 if __name__ == '__main__':
--- packages/try/try-faststream/src/try_faststream/run_sub.py
+++ packages/try/try-faststream/src/try_faststream/run_sub.py
@@ -11,8 +11,19 @@ app = FastStream(broker)
 
 
 @broker.subscriber("test")
-async def base_handler(body):
-    logger.debug(f"subscriber:  {body}")
+async def to_batch(body):
+    """
+    一条消息, 如果有多个实例, 会多次消费(重复消费)
+    """
+    logger.debug(f"subscriber: {body}")
+
+
+@broker.subscriber("test-workers", "workers")
+async def to_one(body):
+    """
+    并发多个 workers 时, 保证一条消息, 只被一个 worker 处理(防止重复消费)
+    """
+    logger.debug(f"handler: {body}")
 
 
 if __name__ == '__main__':
