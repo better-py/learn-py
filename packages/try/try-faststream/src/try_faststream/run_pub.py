e495a9535321165de42e9cb6e6816e1380cfaca5
Henry.Huang <hhglory@outlook.com>
2024-07-08 16:34:25 +0800

    update

--- packages/try/try-faststream/src/try_faststream/run_pub.py
+++ packages/try/try-faststream/src/try_faststream/run_pub.py
@@ -10,13 +10,19 @@ async def pub():
     async with NatsBroker(host) as broker:
         msg = "Hi!"
         logger.debug(f"publish message: {msg}")
+
+        #
+        # todo x: 发布一条消息, 会重复消费
+        #
         await broker.publish(
             msg,
             subject="test",
         )
 
+        # --------------------------------------------------------------------------
+
         #
-        # to one worker:
+        # todo x: 批量发布, 防止重复消费, to one worker:
         #
         for i in range(10):
             msg = f"msg {i}"
@@ -25,6 +31,19 @@ async def pub():
                 msg,
                 subject="test-workers",
             )
+
+        # --------------------------------------------------------------------------
+
+        #
+        # todo x: kv watch 示例
+        #
+        bk = "bucket"
+        key = 'key1'
+        kv = await broker.key_value(bucket=bk)
+        for i in range(4):
+            msg = f"value: {i}"
+            logger.debug(f"Nats KV Store({bk}), put({key}, {msg})")
+            await kv.put(key, msg.encode())
 
 
 if __name__ == '__main__':
--- packages/try/try-faststream/src/try_faststream/run_sub.py
+++ packages/try/try-faststream/src/try_faststream/run_sub.py
@@ -13,7 +13,7 @@ app = FastStream(broker)
 @broker.subscriber("test")
 async def to_batch(body):
     """
-    一条消息, 如果有多个实例, 会多次消费(重复消费)
+    TODO X: 一条消息, 如果有多个实例, 会多次消费(重复消费)
     """
     logger.debug(f"subscriber: {body}")
 
@@ -21,9 +21,28 @@ async def to_batch(body):
 @broker.subscriber("test-workers", "workers")
 async def to_one(body):
     """
-    并发多个 workers 时, 保证一条消息, 只被一个 worker 处理(防止重复消费)
+    TODO X: 并发多个 workers 时, 保证一条消息, 只被一个 worker 处理(防止重复消费)
     """
     logger.debug(f"handler: {body}")
+
+
+@broker.subscriber("key1", kv_watch="bucket")
+async def kv_watch(body):
+    """
+    TODO X: kv watch 示例, 监听 key1 变化, 触发 kv_watch, 会出现重复消费现象!!!
+    """
+    logger.debug(f"Nats KV Store watch kv(key1, {body})")
+
+
+@app.after_startup
+async def setup_broker():
+    """
+    TODO X: kv watch 示例, 预选创建 kv bucket
+    """
+    bk = "bucket"
+    kv = await broker.key_value(bucket=bk)
+    logger.debug(f"Nats startup hook: create kv bucket: {bk}, {kv}")
+    # await kv.put("key1", b"Hello!")
 
 
 if __name__ == '__main__':
