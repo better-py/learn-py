d8c9f722bf6fd9257458bb7c44cda8b2fb2b0bae
hhglory <hhglory@outlook.com>
2024-12-16 20:39:34 +0800

    update

--- packages/py-gui/try-flet/flet-template/main.py
+++ packages/py-gui/try-flet/flet-template/main.py
@@ -1,7 +1,4 @@
 from core.views import app  # todo x: import this!
 
-# ft.app(main, port=8000, view=ft.AppView.WEB_BROWSER)  # run web mode
-# ft.app(main)  # run app mode
-
-# We run the application
+# run
 app.run(port=8000)
