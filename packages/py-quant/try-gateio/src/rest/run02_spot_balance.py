2bb81fcdb3ae74090391badd56bbb741b9f5a982
hhglory <hhglory@outlook.com>
2025-02-19 18:29:30 +0800

    update

---
+++ packages/py-quant/try-gateio/src/rest/run02_spot_balance.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+# example WebSocket signature calculation implementation in Python
+import hashlib
+import hmac
+import time
+from os import getenv
+
+import requests
+from loguru import logger
+
+"""
+
+
+https://www.gate.io/docs/developers/apiv4/en/#list-spot-accounts
+
+
+
+
+"""
+
+
+def gen_sign(method, url, query_string=None, payload_string=None):
+    # GateAPIv4 key pair
+    key = getenv("GATEIO_API_KEY")
+    secret = getenv("GATEIO_API_SECRET")  # required
+
+    logger.debug(f"api key: {key}, secret: {secret}")
+
+    t = time.time()
+    m = hashlib.sha512()
+    m.update((payload_string or "").encode('utf-8'))
+    hashed_payload = m.hexdigest()
+    s = '%s\n%s\n%s\n%s\n%s' % (method, url, query_string or "", hashed_payload, t)
+    sign = hmac.new(secret.encode('utf-8'), s.encode('utf-8'), hashlib.sha512).hexdigest()
+    return {'KEY': key, 'Timestamp': str(t), 'SIGN': sign}
+
+
+def restapi():
+    """
+    todo x: 查询订单列表
+        https://www.gate.io/docs/developers/apiv4/zh_CN/#%E6%9F%A5%E8%AF%A2%E8%AE%A2%E5%8D%95%E5%88%97%E8%A1%A8
+
+    """
+
+    host = "https://api.gateio.ws"
+    prefix = "/api/v4"
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+
+    url = '/spot/accounts'
+    query_param = ''
+    # for `gen_sign` implementation, refer to section `Authentication` above
+    sign_headers = gen_sign('GET', prefix + url, query_param)
+    headers.update(sign_headers)
+    r = requests.request('GET', host + prefix + url, headers=headers)
+    print(r.json())
+
+
+if __name__ == '__main__':
+    restapi()
