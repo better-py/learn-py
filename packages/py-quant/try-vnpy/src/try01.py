7901f1f341c0c17be768bfc200f6ccb6b5cc060d
hhglory <hhglory@outlook.com>
2025-03-03 11:41:00 +0800

    update

--- packages/py-quant/try-vnpy/src/try01.py
+++ packages/py-quant/try-vnpy/src/try01.py
@@ -33,22 +33,30 @@ def main():
     main_engine = MainEngine(event_engine)
 
     # main_engine.add_gateway(CtpGateway)  # TODO X: macos fix, https://github.com/vnpy/vnpy_ctp
-    main_engine.add_app(CtaStrategyApp)
-    main_engine.add_app(CtaBacktesterApp)
-    main_engine.add_app(SpreadTradingApp)
-    main_engine.add_app(OptionMasterApp)
-    main_engine.add_app(PortfolioStrategyApp)
-    main_engine.add_app(AlgoTradingApp)
-    main_engine.add_app(ScriptTraderApp)
-    main_engine.add_app(PaperAccountApp)
-    main_engine.add_app(ChartWizardApp)
-    main_engine.add_app(PortfolioManagerApp)
-    main_engine.add_app(RpcServiceApp)
-    main_engine.add_app(DataManagerApp)
-    main_engine.add_app(DataRecorderApp)
-    main_engine.add_app(ExcelRtdApp)
-    main_engine.add_app(WebTraderApp)
-    main_engine.add_app(RiskManagerApp)
+    main_engine.add_app(CtaStrategyApp)  # CTA策略引擎模块
+    main_engine.add_app(CtaBacktesterApp)  # CTA策略回测模块
+
+    #
+    # ref: https://www.vnpy.com/docs/cn/community/app/spread_trading.html
+    #
+    main_engine.add_app(SpreadTradingApp)  # 价差交易模块，支持自定义价差，实时计算价差行情和持仓，支持价差算法交易以及自动价差策略两种模式
+    main_engine.add_app(OptionMasterApp)  # 期权交易模块
+    main_engine.add_app(PortfolioStrategyApp)  # 组合策略模块，面向同时交易多合约的量化策略（Alpha、期权套利等）
+    main_engine.add_app(AlgoTradingApp)  # 算法交易模块，提供多种常用的智能交易算法：TWAP、Sniper、Iceberg、BestLimit等
+    main_engine.add_app(ScriptTraderApp)  # 脚本策略模块
+    main_engine.add_app(PaperAccountApp)  # 本地仿真模块，纯本地化实现的仿真模拟交易功
+
+    #
+    # ref: https://www.vnpy.com/docs/cn/community/app/chart_wizard.html
+    #
+    main_engine.add_app(ChartWizardApp)  # K线图表模块
+    main_engine.add_app(PortfolioManagerApp)  # 交易组合管理模块
+    main_engine.add_app(RpcServiceApp)  # RPC服务模块
+    main_engine.add_app(DataManagerApp)  # 历史数据管理模块
+    main_engine.add_app(DataRecorderApp)  # 行情记录模块
+    main_engine.add_app(ExcelRtdApp)  # Excel RTD（Real Time Data）实时数据服务，基于pyxll模块实现在Excel中获取各类数据（行情、合约、持仓等）的实时推送更新
+    main_engine.add_app(WebTraderApp)  # Web服务模块
+    main_engine.add_app(RiskManagerApp)  # 风险管理模块
 
     #
     # QT GUI Window
