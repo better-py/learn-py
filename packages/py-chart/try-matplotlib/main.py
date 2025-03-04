2729418e42bdbc66ac01db2408ceefeefb074c6b
hhglory <hhglory@outlook.com>
2025-03-04 21:28:15 +0800

    update

---
+++ packages/py-chart/try-matplotlib/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from try-matplotlib!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/py-chart/try-pyecharts/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from try-pyecharts!")
+
+
+if __name__ == "__main__":
+    main()
