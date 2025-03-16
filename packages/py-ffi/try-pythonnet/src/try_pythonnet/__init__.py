79d5c635f73b91236d9dca6e3c92dfc4668b0808
hhglory <hhglory@outlook.com>
2025-03-16 11:18:51 +0800

    update

--- packages/py-ffi/try-pythonnet/src/try_pythonnet/__init__.py
+++ packages/py-ffi/try-pythonnet/src/try_pythonnet/__init__.py
@@ -1,2 +1,10 @@
+import pythonnet
+
+
 def hello() -> str:
+    print(f"pythonet all: {pythonnet.__all__}")
     return "Hello from try-pythonnet!"
+
+
+if __name__ == "__main__":
+    print(hello())
